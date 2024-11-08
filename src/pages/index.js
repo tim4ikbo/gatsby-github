72e2fbed6527f1c9a001908f3c4f461efe171f7c
Timofey Bobkov <bobkov.star.tima@gmail.com>
2024-11-09 00:13:04 +0200

    Changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -19,9 +19,7 @@ export default (() => {
 			<Text as="h2" font="--headline1" md-font="--headline2" margin="20px 0 0 0">
 				About Us
 			</Text>
-			<Text as="p" font="--lead" margin="20px 0 20px 0" text-align="center">
-				Hi! I'm a paragraph. Click here to add your own text and edit me. It’s a piece of cake. I’m a great space for you to tell a story and let your site visitors know more about you. Talk about your business and what products and services you offer. Share how you came up with the idea for your company and what makes you different from your competitors. Make your business stand out and show your visitors who you are.
-			</Text>
+			<Text as="p" font="--lead" margin="20px 0 20px 0" text-align="center" />
 			<Button font="--lead" margin="20px">
 				Button
 			</Button>
